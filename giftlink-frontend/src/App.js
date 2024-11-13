0bc82299bd4a264cb32ca743262252746ce44f5c
tanvirAhmed73 <tanvir.073.ahmed@gmail.com>
2024-11-13 05:59:19 -0500

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,7 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -15,6 +16,7 @@ function App() {
         <Navbar/>
         <Routes>
           {/* the final code will not pass the products to every page, but each page will call the server API */}
+          <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -6,7 +6,9 @@ export default function Navbar() {
             <a className="navbar-brand" href="/">GiftLink</a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/search">Search</Link>
+                    </li>
                     <li className="nav-item">
                     <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
                     </li>
